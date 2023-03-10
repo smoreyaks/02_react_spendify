fb0b271e89860854112ae543736c806b3e518b68
smoreyaks <bnixon.eng@gmail.com>
2023-03-11 00:33:32 +1100

    Header w/ Styling & State - Needs MobNav/Nav Comp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,74 @@
-import React from 'react';
+// React Packages
+import React, { useState, useEffect } from 'react';
+
+// Data
+import { header } from '../data';
+
+// Icons
+import { HiOutlineX, HiMenuAlt4 } from 'react-icons/hi';
+
+// Components
+import MobileNav from '../components/MobileNav'
+import Nav from '../components/Nav'
 
 const Header = () => {
-  return <div>Header</div>;
+  // Mobile Navigation State
+  const [mobileNav, setMobileNav] = useState(false);
+  // Header State
+  const [isActive, setIsActive] = useState(false);
+  // Destructure Header Data
+  const { logo, btnText } = header;
+  
+  // Scroll Event
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      window.scrollY > 60 ?    setIsActive(true) : setIsActive(false);
+    });
+  })
+  
+  return (
+    <header 
+      className={`${isActive ? 'lg:top-0 bg-white shadow-2xl' : 'lg:top-[60px]'} py-6 lg:py-4 fixed w-full transition-all z-10`}
+    >
+      <div className='container mx-auto flex justify-between items-center'>
+        
+        {/* Logo */}
+        <a href="#">
+          <img src={logo} alt="" />
+        </a>
+        
+        {/* Navigation - Hidden on Mobile, shows on Desktop */}
+        <div className="hidden lg:flex">
+          <Nav />
+        </div>
+        
+        {/* CTA Button - Hidden on Mobile, shows on Deskop  */}
+        <button className='btn btn-sm btn-outline hidden lg:flex'>
+          {btnText}
+        </button>
+        
+        {/* Mobile Nav Trigger Button - Hidden on Mobile, shows on Desktop */}
+        <button 
+          className='lg:hidden' 
+          onClick={() => setMobileNav(!mobileNav)}
+        >
+          { mobileNav ? (
+            <HiOutlineX className='text-3xl text-accent' />
+          ) : (
+            <HiMenuAlt4 className='text-3xl text-accent' />
+          )}
+        </button>
+        {/* Mobile Navigation - Hidden on Mobile, shows on Desktop */}
+        <div className={`${
+          mobileNav ? 'left-0' : '-left-full'
+        } fixed top-0 bottom-0 bg-pink-400 w-[60vw] lg:hidden transition-all `}
+        >
+          <MobileNav />
+        </div>
+
+      </div>
+    </header>
+  )
 };
 
 export default Header;
