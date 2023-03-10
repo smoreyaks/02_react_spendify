bd1da055b36e629ba80a2b719d5603385fd487f9
smoreyaks <bnixon.eng@gmail.com>
2023-03-10 18:16:54 +1100

    Hero Section Styled & Animated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,60 @@
-import React from 'react';
+import React from "react";
 
 // Import data
-import { hero } from '../data'
+import { hero } from "../data";
 
 // Import Icons
-import { HiOutlineChevronDown } from 'react-icons/hi'
+import { HiOutlineChevronDown } from "react-icons/hi";
 
 const Hero = () => {
+  
   // Destructure Hero Data
   const { title, subtitle, btnText, compText, image } = hero;
   return (
-      <section className='bg-pink-200 min-h-[900px] py-12'>
-        <div className='container mx-auto min-h-[900px] flex justify-center items-center'> 
-          <div className="flex flex-col lg:gap-x-[30px] gap-y-8"></div>
-          {/* Text */}
-          <div className="flex-1">Text</div>
+    <section className="min-h-[900px] py-12">
+      <div className="container mx-auto min-h-[900px] flex justify-center items-center">
+        <div className="flex flex-col lg:gap-x-[30px] gap-y-8 lg:gap-y-0 lg:flex-row items-center justify-center text-center lg:text-left">
+
+          {/* Hero Title Text */}
+          <div className="flex-1">
+            <h1
+              className="title mb-2 lg:mb-5"
+              data-aos="fade-down"
+              data-aos-delay="400"
+            >
+              {title}
+            </h1>
+
+            {/* Subtitle Text */}
+            <p
+              className="lead mb-5 lg:mb-10"
+              data-aos="fade-down"
+              data-aos-delay="800"
+            >
+              {subtitle}
+            </p>
+
+            {/* Btn & Comp Text */}
+            <div
+              className="flex items-center max-w-sm lg:max-w-full mx-auto lg:mx-0 gap-x-2 lg:gap-x-6"
+              data-aos="fade-down"
+              data-aos-delay="1300"
+            >
+              <button className="btn btn-md lg:btn-lg btn-accent flex justify-center items-center">
+                {btnText}
+              </button>
+              <span className="text-light lg:lead lg:mb-0">{compText}</span>
+            </div>
+          </div>
+
+          {/* Image */}
+          <div className="flex-1" data-aos="fade-up" data-aos-delay="1700">
+            <img src={image} alt="" />
+          </div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default Hero;
