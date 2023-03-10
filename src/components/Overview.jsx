d6bf60ab23cb5561475c2c076ed8fae20a786d95
smoreyaks <bnixon.eng@gmail.com>
2023-03-11 01:36:46 +1100

    Overview Chart Section w/ Animation

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,7 +1,24 @@
+// React Packages
 import React from 'react';
 
+// Data
+import { overview } from '../data';
+
 const Overview = () => {
-  return <div>Overview</div>;
+  // Destructure Overview Data
+  const { productImg } = overview;
+  return (
+    <section className='lg:min-h-[712px] bg-overview bg-cover bg-left-top pt-[30px] lg:pt-[87px]'>
+      <div className='container mx-auto flex justify-end overflow-hidden'>
+        <img 
+          src={productImg} 
+          alt=""
+          data-aos='fade-up'
+          data-aos-offset='300'
+        />
+      </div>
+    </section>
+  )
 };
 
 export default Overview;
