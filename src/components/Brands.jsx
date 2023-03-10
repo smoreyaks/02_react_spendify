c996b1c6172637b42b0eef6060e10c56e2ce60ac
smoreyaks <bnixon.eng@gmail.com>
2023-03-11 02:38:16 +1100

    Brands - Styling & Animation

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -1,7 +1,30 @@
+// React Packages
 import React from 'react';
 
+// Data
+import { overview } from '../data'
+
 const Brands = () => {
-  return <div>Brands</div>;
+  // Destructure Overview
+  const { brands } = overview;
+  
+  return ( 
+    <section className='py-9'>
+      <div className="container mx-auto flex flex-col lg:flex-row items-center justify-between space-y-6">
+        {/* Brands */}
+        { brands.map((item, index) => {
+          // Destructure Item
+          const { image, delay } = item;
+          return (
+            <div key={ index } data-aos='fade-up' data-aos-delay={ delay }>
+              {/* Brand Image */}
+              <img src={ image } alt="" />
+            </div>
+          )
+        })}
+      </div>
+    </section>
+  )
 };
 
 export default Brands;
