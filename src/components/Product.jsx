ce251808a1fde6dd019f160ee280b2ff49def01b
smoreyaks <bnixon.eng@gmail.com>
2023-03-12 17:29:17 +1100

    Product - Layout, Styling & Animation

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,42 @@
+// React Packages
 import React from 'react';
 
+// Data
+import { product } from '../data'
+// Components
+import Cards from './Cards'
+
 const Product = () => {
-  return <div>Product</div>;
+  // Destructure Product Data
+  const { title, subtitle } = product;
+  
+  return  (
+    <section>
+      <div className="container mx-auto">
+        
+        {/* Title & Subtitle */}
+        <div className='flex flex-col items-center lg:flex-row mb-10 lg:mb-20'>
+          <h2 
+            className='section-title' 
+            data-aos='fade-up' 
+            data-aos-offset='400' 
+            data-aos-delay='400'
+          >
+            { title }
+          </h2>
+          <p className='lead lg:max-w-[350px]' 
+            data-aos='fade-up' 
+            data-aos-offset='400' 
+            data-aos-delay='400'
+          >
+            { subtitle }
+          </p>
+        </div>
+        {/* Cards */}
+        <Cards />
+      </div>
+    </section>
+  )
 };
 
 export default Product;
