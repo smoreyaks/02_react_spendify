86a1eb81894f327bc9bf55cb71ee2c9e270bd59b
smoreyaks <bnixon.eng@gmail.com>
2023-03-13 16:22:17 +1100

    Testimonials - Layout, Styling, Animations

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,34 @@
+// React Packages
 import React from 'react';
 
+// Data
+import { testimonials } from '../data';
+
+// Components
+import ClientSlider from '../components/ClientSlider'
+
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  // Destructure Testimonials
+  const { title, clients } = testimonials;
+  return (
+    <section className='section'>
+      <div className="container mx-auto">
+
+        {/* Title */}
+        <h2 
+          className="title mb-10 lg:mb-20 text-center max-w-[920px] mx-auto"
+          data-aos='fade-up'
+          data-aos-delay='200'
+        >
+          { title }
+        </h2>
+        {/* Slider */}
+        <div data-aos='fade-up' data-aos-delay='400'>
+          <ClientSlider clients={ clients }/>
+        </div>
+      </div>
+    </section>
+  )
 };
 
 export default Testimonials;
