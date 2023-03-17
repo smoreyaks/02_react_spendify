4888a088713011d85f14d9a04d41a26e12516074
smoreyaks <bnixon.eng@gmail.com>
2023-03-18 04:10:26 +1100

    Changed BG to Colour Grad

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -8,7 +8,7 @@ const Overview = () => {
   // Destructure Overview Data
   const { productImg } = overview;
   return (
-    <section className='lg:min-h-[712px] bg-overview bg-cover bg-left-top pt-[30px] lg:pt-[87px]'>
+    <section className='lg:min-h-[712px] bg-gradient-to-br from-blue-300 via-orange-300 to-red-400 bg-cover bg-left-top pt-[30px] lg:pt-[87px]'>
       <div className='container mx-auto flex justify-end overflow-hidden'>
         <img 
           src={productImg} 
