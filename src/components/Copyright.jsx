6949e814a7a75e156a8933cb47b9138def817ab5
smoreyaks <bnixon.eng@gmail.com>
2023-03-18 21:56:49 +1100

    Modified Footer Layout to Horizontal First

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -9,32 +9,38 @@ const Copyright = () => {
   const { link1, link2, copyText, social } = copyright;
   return (
     <div 
-      className='flex flex-col items-center gap-y-2 lg:flex lg:justify-between'
+      className='flex flex-col lg:flex-row items-center gap-y-2 lg:flex lg:justify-between lg:px-6'
       data-aos='fade-up'
       data-aos-offset=' 0'
       data-aos-delay='200'
     >
+      
       {/* links */}
-      <div className='flex gap-x-6'>
-        <a className='hover:text-accent transition' href={ link2.href }>{ link2.name }</a>
-        <a className='hover:text-accent transition' href={ link1.href }>{ link1.name }</a>
+      <div className='flex gap-x-6 justify-between flex-col lg:flex-row items-center'>
+        <a className='hover:text-accent transition font-semibold mb-3 lg:mb-0' href={ link1.href }>{ link1.name }</a>
+        <a className='hover:text-accent transition font-semibold mb-3 lg:mb-0' href={ link2.href }>{ link2.name }</a>
+      
+        {/* Copyright Text */}
+        <div className='mb-3 lg:mb-0 font-semibold'>{ copyText }</div>
+
+        {/* Social Icons */}
+        <ul className='flex gap-x-[12px] justify-center'>
+          { social.map((item, index) => {
+            
+            // Destructure Item
+            const { href, icon } = item
+            return (
+              <li key={ index }>
+                <a href={ href }>
+                  <img className='h-6' src={icon} alt="" />
+                </a>
+              </li>
+            )
+          })}
+        </ul>
       </div>
-      {/* Copyright Text */}
-      <div>{copyText}</div>
-      {/* Social Icons */}
-      <ul className='flex gap-x-[12px]'>
-        { social.map((item, index) => {
-          // Destructure Item
-          const { href, icon } = item
-          return (
-            <li key={ index }>
-              <a href={ href }>
-                <img src={icon} alt="" />
-              </a>
-            </li>
-          )
-        })}
-      </ul>
+      
+      
     </div>
   )
 };
