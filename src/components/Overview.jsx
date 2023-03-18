63f829267b95a71911b861d268633240da8919d6
smoreyaks <bnixon.eng@gmail.com>
2023-03-18 22:12:03 +1100

    Added Content Box Style, AOS & Color Grad

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -8,8 +8,12 @@ const Overview = () => {
   // Destructure Overview Data
   const { productImg } = overview;
   return (
-    <section className='lg:min-h-[712px] bg-gradient-to-br from-blue-300 via-orange-300 to-red-400 bg-cover bg-left-top pt-[30px] lg:pt-[87px]'>
-      <div className='container mx-auto flex justify-end overflow-hidden'>
+    <section className='lg:min-h-[712px] from-white via-orange-300 to-red-400 bg-left-top pt-[30px] lg:pt-[87px]'>
+      <div className='container mx-auto flex justify-end items-center overflow-hidden contentBox' data-aos="fade-up"
+              data-aos-delay="200"
+            >
+      {/* <a href="https://www.vecteezy.com/free-vector/background"></a> */}
+      {/* Background Vectors by Vecteezy */}
         <img 
           src={productImg} 
           alt=""
