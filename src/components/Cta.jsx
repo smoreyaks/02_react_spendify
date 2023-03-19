294ba33e604630172178912827493977a59a992a
smoreyaks <bnixon.eng@gmail.com>
2023-03-19 22:05:49 +1100

    Text Black, Button Hover & Reduced Container Size

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -11,22 +11,22 @@ const Cta = () => {
   // Destructure CTA Data
   const { title, subtitle, btnText, img1, img2} = cta
   return (
-    <section className='section bg-gradient-to-br from-blue-300 via-orange-300 to-red-400 bg-cover bg-left-top'>
-      <div className='container max-w-[1340px] mx-auto px-[25px] contentBox' data-aos="fade-up"
+    <section className='section'>
+      <div className='container  mx-auto contentBox' data-aos="fade-up"
               data-aos-delay="200"
-            >
+      >
         
         {/* Text */}
         <div className='max-w-[920px] mx-auto text-center'>
           <h2 
-            className="h2 text-white mb-6"
+            className="h2 text-black mb-6"
             data-aos='fade-up'
             data-aos-delay='200'
           >
             { title }
           </h2>
           <p 
-            className='text-2xl lg:text-4xl text-white'
+            className='text-2xl lg:text-4xl text-black'
             data-aos='fade-up'
             data-aos-delay='300'>
             {subtitle}
@@ -39,7 +39,7 @@ const Cta = () => {
               data-aos-delay='700'
             />
             <button 
-              className='btn btn-md btn-white mt-[40px] lg:text-[22px] gap-x-[10px] mx-auto md:min-w-[200px]'
+              className='btn btn-outline btn-md btn-white mt-[40px] lg:text-[22px] gap-x-[10px] mx-auto md:min-w-[200px]'
               data-aos='fade-up'
               data-aos-delay='400'
             >
