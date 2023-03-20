3846455dcc597114a6a117bf3e64d850554b362d
smoreyaks <bnixon.eng@gmail.com>
2023-03-20 17:15:16 +1100

    Title Row Layout, Btn Style, Card Width & 4th Row

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -18,7 +18,7 @@ const Pricing = () => {
     <section className='section'>
       <div className="container mx-auto contentBox" data-aos="fade-up"
               data-aos-delay="200"
-            >
+      >
         
         {/* Title */}
         <h2 className="h2 mb-10 lg:mb-20 text-center px-10" 
@@ -29,7 +29,7 @@ const Pricing = () => {
         </h2>
         
         {/* Cards */}
-        <div className='flex flex-col lg:flex-row lg:gap-x-[30px] gap-y-[30px] lg:gap-y-0 justify-center items-center'>
+        <div className='flex flex-col lg:flex-row lg:gap-x-[30px] gap-y-[30px] lg:gap-y-0 justify-center items-center text-'>
             { cards.map((card, cardIndex)=> {
               // Destructure Card
               const {icon, title, services, price, userAmount, btnText, delay } = card;
@@ -46,16 +46,19 @@ const Pricing = () => {
                     onClick={() => setIndex(cardIndex)}
                     className={`${
                       cardIndex === index ? 'bg-white shadow-2xl' : 'shadow-md' } 
-                      w-[350px] h-[500px] rounded-[12px] p-[40px] cursor-pointer transition-all bg-white`}
+                      w-[300px] h-[500px] rounded-[12px] p-[40px] cursor-pointer transition-all bg-white`}
                   >
-                    
-                    {/* Card Icon */}
-                    <div className='mb-8'>
-                      <img src={ icon } alt="" />
+                    {/* Card Title Row */}
+                    <div className='flex flex-row justify-between'>
+                      
+                      {/* Card Title */}
+                      <div className='text-2xl font-semibold mb-8'>{ title }</div>
+                      
+                      {/* Card Icon */}
+                      <div className='mb-8'>
+                        <img src={ icon } alt="" />
+                      </div>
                     </div>
-                    
-                    {/* Card Title */}
-                    <div className='text-[32px] font-semibold mb-8'>{ title }</div>
                     
                     {/* Card Services */}
                     <div className='flex flex-col gap-y-2 mb-6'>
@@ -65,20 +68,19 @@ const Pricing = () => {
                         const { name } = service;
                         return (
                           <div 
-                            className='flex items-center gap-x-[10px]' 
+                            className='flex justify-start items-center gap-x-[10px]' 
                             key={index}
                           >
                               <HiCheck className='text-light' />
                               <div>{name}</div>
-                            </div>
+                          </div>
                         )
                       })}
-                      <div className='mb-10'>
-                        <div className=''>
+                      <div className='mb-10 py-4'>
+                        <div className='flex justify-start'>
                           <span className='text-2xl font-semibold'>{ price }/</span>
-                          <span className='text-xl text-light font-light'> year </span>
+                          <span className='text-2xl text-light font-light'> Month </span>
                         </div>
-                      <div className='text-base text-light'>{ userAmount }</div>
                       </div>
                     </div>
                     
@@ -87,7 +89,7 @@ const Pricing = () => {
                       className={`${
                         cardIndex === index
                           ? 'bg-accent hover:bg-accentHover text-white'
-                          : 'border border-accent text-accent'
+                          : 'border border-accent text-accent btn-outline'
                       } btn btn-sm space-x-[14px]`}
                     >
                       <span>{ btnText }</span>
