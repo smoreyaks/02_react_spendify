0e13a5ead30e7c635f3bbf595dbebc0afa2fdb4e
smoreyaks <bnixon.eng@gmail.com>
2023-03-25 14:27:01 +1100

    Reduced Product Card Min Width

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,65 +1,65 @@
 // React Components
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 // Data
-import { product } from '../data'
+import { product } from "../data";
 
 // Images
-import ArrowImg from '../assets/img/product/cards/arrow.svg'
+import ArrowImg from "../assets/img/product/cards/arrow.svg";
 
 const Cards = () => {
-  
-  // Index State
-  const [index, setIndex] = useState(1);
-  
-  // Destructure Product Data
-  const { cards } = product;
+    // Index State
+    const [index, setIndex] = useState(1);
 
-  return (
-    <>
-      {/* Cards */}
-      <div className="flex flex-col gap-y-[30px] lg:flex-row lg:gap-x-[30px] justify-around">
-      
-      { cards.map((card, cardIndex) => {
-        
-        // Destructure Card
-        const { icon, title, subtitle, delay } = card;
-        return (
-          <div 
-            key={cardIndex} 
-            data-aos='zoom-out' 
-            data-aos-offset='300' 
-            data-aos-delay='delay'
-          >
-            <div
-              onClick={() => setIndex(cardIndex)}
-              className={`${
-                index === cardIndex ? 'bg-white shadow-2xl' : 'shadow-md'
-              }
-                w-[300px] h-[350px] flex flex-col justify-between items-center mx-auto py-[50px] px-[50px] text-center rounded-[12px] cursor-pointer transition-all bg-white`}
-            > 
+    // Destructure Product Data
+    const { cards } = product;
 
-              {/* Card Icon */}
-              <div className='mb-6'>
-                <img src={ icon } alt="" />
-              </div>
+    return (
+        <>
+            {/* Cards */}
+            <div className="flex flex-col gap-y-[30px] lg:flex-row lg:gap-x-[30px] justify-around">
+                {cards.map((card, cardIndex) => {
+                    // Destructure Card
+                    const { icon, title, subtitle, delay } = card;
+                    return (
+                        <div
+                            key={cardIndex}
+                            data-aos="zoom-out"
+                            data-aos-offset="300"
+                            data-aos-delay="delay"
+                        >
+                            <div
+                                onClick={() => setIndex(cardIndex)}
+                                className={`${
+                                    index === cardIndex
+                                        ? "bg-white shadow-2xl"
+                                        : "shadow-md"
+                                }
+                              min-w-[230px] h-[350px] flex flex-col justify-between items-center mx-auto py-[50px] px-[50px] text-center rounded-[12px] cursor-pointer transition-all bg-white`}
+                            >
+                                {/* Card Icon */}
+                                <div className="mb-6">
+                                    <img src={icon} alt="" />
+                                </div>
 
-              {/* Card Title */}
-              <div className='mb-3 text-[30px] font-medium'>{ title }</div>
+                                {/* Card Title */}
+                                <div className="mb-3 text-[30px] font-medium">
+                                    {title}
+                                </div>
 
-              {/* Card Subtitles */}
-              <p className='mb-6 text-light'>{ subtitle }</p>
+                                {/* Card Subtitles */}
+                                <p className="mb-6 text-light">{subtitle}</p>
 
-              {/* Arrow Title */}
-              { index === cardIndex && <img src={ ArrowImg }/>}
-              <div></div>
+                                {/* Arrow Title */}
+                                {index === cardIndex && <img src={ArrowImg} />}
+                                <div></div>
+                            </div>
+                        </div>
+                    );
+                })}
             </div>
-          </div>
-        )
-      })}
-      </div>
-    </>
-  ) 
+        </>
+    );
 };
 
 export default Cards;
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,44 +1,46 @@
 // React Packages
-import React from 'react';
+import React from "react";
 
 // Data
-import { product } from '../data'
+import { product } from "../data";
 // Components
-import Cards from './Cards'
+import Cards from "./Cards";
 
 const Product = () => {
-  // Destructure Product Data
-  const { title, subtitle } = product;
-  
-  return  (
-    <section className='section'>
-      <div className="container mx-auto contentBox" data-aos="fade-up"
-              data-aos-delay="200"
+    // Destructure Product Data
+    const { title, subtitle } = product;
+
+    return (
+        <section className="section">
+            <div
+                className="container mx-auto contentBox"
+                data-aos="fade-up"
+                data-aos-delay="200"
             >
-        
-        {/* Title & Subtitle */}
-        <div className='flex flex-col justify-between items-center mb-10'>
-          <h2 
-            className='title' 
-            data-aos='fade-up' 
-            data-aos-offset='400' 
-            data-aos-delay='400'
-          >
-            { title }
-          </h2>
-          <p className='lead' 
-            data-aos='fade-up' 
-            data-aos-offset='400' 
-            data-aos-delay='400'
-          >
-            { subtitle }
-          </p>
-        </div>
-        {/* Cards */}
-        <Cards />
-      </div>
-    </section>
-  )
+                {/* Title & Subtitle */}
+                <div className="flex flex-col justify-between items-center mb-10">
+                    <h2
+                        className="title"
+                        data-aos="fade-up"
+                        data-aos-offset="400"
+                        data-aos-delay="400"
+                    >
+                        {title}
+                    </h2>
+                    <p
+                        className="lead"
+                        data-aos="fade-up"
+                        data-aos-offset="400"
+                        data-aos-delay="400"
+                    >
+                        {subtitle}
+                    </p>
+                </div>
+                {/* Cards */}
+                <Cards />
+            </div>
+        </section>
+    );
 };
 
 export default Product;
