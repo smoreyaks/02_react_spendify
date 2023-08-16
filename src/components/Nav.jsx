55740a833f0b1063b0999241def92f7d4e263b24
smoreyaks <bnixon.eng@gmail.com>
2023-08-17 04:02:10 +1000

    Changed Nav Links to Semibold

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,31 +1,30 @@
 // React Packages
-import React from 'react';
+import React from "react";
 
 // Data
-import { nav } from '../data'
-
-
+import { nav } from "../data";
 
 const Nav = () => {
-
-  return (
-    <nav>
-      <ul className='flex gap-x-10'>
-        { nav.map((item, index) => {
-
-          // Destructure Item 
-          const { href, name } = item;
-          return (
-            <li key={index}>
-              <a className='hover:text-accent transition' href={href}>
-                { name }
-              </a>
-            </li>
-          );
-        })}
-      </ul>    
-    </nav>
-  )
+    return (
+        <nav>
+            <ul className="flex gap-x-10">
+                {nav.map((item, index) => {
+                    // Destructure Item
+                    const { href, name } = item;
+                    return (
+                        <li key={index}>
+                            <a
+                                className="hover:text-accent font-semibold transition"
+                                href={href}
+                            >
+                                {name}
+                            </a>
+                        </li>
+                    );
+                })}
+            </ul>
+        </nav>
+    );
 };
 
 export default Nav;
