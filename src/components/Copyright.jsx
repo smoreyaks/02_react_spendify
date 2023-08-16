6a6559e6c2d3913d876132091066e0a8c7685334
smoreyaks <bnixon.eng@gmail.com>
2023-08-17 03:56:52 +1000

    Added SVG Accent (Hover) Class

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -1,48 +1,58 @@
 // React Packages
-import React from 'react';
+import React from "react";
 
 // Data
-import { copyright } from '../data';
+import { copyright } from "../data";
 
 const Copyright = () => {
-  // Destructure Copyright Data
-  const { link1, link2, copyText, social } = copyright;
-  return (
-    <div 
-      className='flex flex-col lg:flex-row items-center gap-y-2 lg:flex lg:justify-between lg:px-6'
-      data-aos='fade-up'
-      data-aos-offset=' 0'
-      data-aos-delay='1300'
-    >
-      
-      {/* links */}
-      <div className='flex gap-x-6 justify-between flex-col lg:flex-row items-center'>
-        <a className='hover:text-accent transition font-semibold mb-3 lg:mb-0' href={ link1.href }>{ link1.name }</a>
-        <a className='hover:text-accent transition font-semibold mb-3 lg:mb-0' href={ link2.href }>{ link2.name }</a>
-      
-        {/* Copyright Text */}
-        <div className='mb-3 lg:mb-0 font-semibold'>{ copyText }</div>
-
-        {/* Social Icons */}
-        <ul className='flex gap-x-[12px] justify-center'>
-          { social.map((item, index) => {
-            
-            // Destructure Item
-            const { href, icon } = item
-            return (
-              <li key={ index }>
-                <a href={ href }>
-                  <img className='h-6' src={icon} alt="" />
+    // Destructure Copyright Data
+    const { link1, link2, copyText, social } = copyright;
+    return (
+        <div
+            className="flex flex-col lg:flex-row items-center gap-y-2 lg:flex lg:justify-between lg:px-6"
+            data-aos="fade-up"
+            data-aos-offset=" 0"
+            data-aos-delay="1300"
+        >
+            {/* links */}
+            <div className="flex gap-x-6 justify-between flex-col lg:flex-row items-center">
+                <a
+                    className="hover:text-accent transition font-semibold mb-3 lg:mb-0"
+                    href={link1.href}
+                >
+                    {link1.name}
                 </a>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-      
-      
-    </div>
-  )
+                <a
+                    className="hover:text-accent transition font-semibold mb-3 lg:mb-0"
+                    href={link2.href}
+                >
+                    {link2.name}
+                </a>
+
+                {/* Copyright Text */}
+                <div className="mb-3 lg:mb-0 font-semibold">{copyText}</div>
+
+                {/* Social Icons */}
+                <ul className="flex gap-x-[12px] justify-center">
+                    {social.map((item, index) => {
+                        // Destructure Item
+                        const { href, icon } = item;
+                        return (
+                            <li key={index}>
+                                <a href={href}>
+                                    <img
+                                        className="h-6 svg-accent"
+                                        src={icon}
+                                        alt=""
+                                    />
+                                </a>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default Copyright;
