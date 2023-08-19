1ceccdbb792071a6630a2d6d3928994231aa4143
smoreyaks <bnixon.eng@gmail.com>
2023-08-19 19:23:17 +1000

    Added More Y Padding To Separate Sections

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,31 +1,33 @@
 // React Packages
-import React from 'react';
+import React from "react";
 
 // Data
-import { overview } from '../data';
+import { overview } from "../data";
 
 // Components
-import Brands from './Brands'
+import Brands from "./Brands";
 
 const Overview = () => {
-  // Destructure Overview Data
-  const { productImg, title } = overview;
-  return (
-    <section className='lg:min-h-[712px] pt-[30px] lg:pt-[87px] section'>
-      <div className='container mx-auto flex flex-col justify-center items-center overflow-hidden contentBox' data-aos="fade-up"
-              data-aos-delay="200"
-              >
-        <h2 className='title flex text-center'>{ title }</h2>
-        <Brands />
-        {/* <img 
-          src={productImg} 
-          alt=""
-          data-aos='fade-up'
-          data-aos-offset='300'
-        /> */}
-      </div>
-    </section>
-  )
+    // Destructure Overview Data
+    const { productImg, title } = overview;
+    return (
+        <section className="lg:min-h-[712px] pt-[30px] lg:py-[266px] section">
+            <div
+                className="container mx-auto flex flex-col justify-center items-center overflow-hidden contentBox"
+                data-aos="fade-up"
+                data-aos-delay="200"
+            >
+                <h2 className="title flex text-center">{title}</h2>
+                <Brands />
+                {/* <img
+                    src={productImg}
+                    alt=""
+                    data-aos="fade-up"
+                    data-aos-offset="300"
+                /> */}
+            </div>
+        </section>
+    );
 };
 
 export default Overview;
