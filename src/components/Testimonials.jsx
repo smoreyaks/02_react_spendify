bf6dfb605192d1bbbac6a543be194c93127b29d6
smoreyaks <bnixon.eng@gmail.com>
2023-09-11 23:00:12 +1000

    Set section height of View Port & centered section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,36 +1,37 @@
 // React Packages
-import React from 'react';
+import React from "react";
 
 // Data
-import { testimonials } from '../data';
+import { testimonials } from "../data";
 
 // Components
-import ClientSlider from '../components/ClientSlider'
+import ClientSlider from "../components/ClientSlider";
 
 const Testimonials = () => {
-  // Destructure Testimonials
-  const { title, clients } = testimonials;
-  return (
-    <section className='section'>
-      <div className="container mx-auto contentBox" data-aos="fade-up"
-              data-aos-delay="200"
+    // Destructure Testimonials
+    const { title, clients } = testimonials;
+    return (
+        <section className="section lg:min-h-[100vh] items-center">
+            <div
+                className="container mx-auto contentBox"
+                data-aos="fade-up"
+                data-aos-delay="200"
             >
-
-        {/* Title */}
-        <h2 
-          className="title mb-10 lg:mb-20 text-center max-w-[920px] mx-auto"
-          data-aos='fade-up'
-          data-aos-delay='200'
-        >
-          { title }
-        </h2>
-        {/* Slider */}
-        <div data-aos='fade-up' data-aos-delay='400'>
-          <ClientSlider clients={ clients }/>
-        </div>
-      </div>
-    </section>
-  )
+                {/* Title */}
+                <h2
+                    className="title mb-10 lg:mb-20 text-center max-w-[920px] mx-auto"
+                    data-aos="fade-up"
+                    data-aos-delay="200"
+                >
+                    {title}
+                </h2>
+                {/* Slider */}
+                <div data-aos="fade-up" data-aos-delay="400">
+                    <ClientSlider clients={clients} />
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Testimonials;
